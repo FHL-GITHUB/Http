6d35cc9dfec234f0cbf89520b634382e4c4c0113
FHL-GITHUB <55864600+FHL-GITHUB@users.noreply.github.com>
2021-03-24 10:24:18 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,61 +8,9 @@ import 'package:try_http/services/api_services.dart';
 import 'package:try_http/model/carpark.dart';
 import 'package:flutter/material.dart';
 import 'package:try_http/page/SearchPage.dart';
-//import 'package:try_http/page/search_page.dart.dart';
 
 void main(){
-  loadCarparkList();
+  //loadCarparkList();
   carparkList();
   runApp(SearchApp());
-  //loadData();
-  //fetchData();
-
 }
-
-
-/*
-class MyApp extends StatelessWidget {
-  List<Records> records = <Records>[];
-  //final Future<List<Records>> records;
-
-  MyApp({this.records});
-  void initState(){
-    print('hereeeeeeeeeeeeeeeeeeee1234512345');
-    addRecords();
-  }
-
-  Widget build(BuildContext context) => Scaffold(
-    appBar: AppBar(
-      centerTitle: true,
-      title: Text('Top Beers'),
-    ),
-    body: ListView.builder(
-      itemCount: records.length,
-      itemBuilder: (context, index) => (BeerTile(records[index])),
-    ),
-  );
-
-  void addRecords() async{
-    final List<Records> futureRecord = await fetchData();
-    setState(){
-      records.addAll(futureRecord);
-    }
-
-  }
-}
-
-class BeerTile extends StatelessWidget {
-  final Records _record;
-  BeerTile(this._record);
-
-  @override
-  Widget build(BuildContext context) => Column(
-    children: <Widget>[
-      ListTile(
-        title: Text(_record.address),
-        subtitle: Text(_record.carParkType),
-      ),
-      Divider()
-    ],
-  );
-}*/
